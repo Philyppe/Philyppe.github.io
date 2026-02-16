b839e5ad54099ff9562edf3fd6d33ef9b99053c3
Felipe <lopezalmanzafelipe@gmail.com>
2026-02-15 20:32:50 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://http://philyppe.github.io',
+    site: 'http://philyppe.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
