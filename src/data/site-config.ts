c3c333d063ba0f985a733efe023707de7129d646
Felipe <lopezalmanzafelipe@gmail.com>
2026-02-15 21:05:34 -0500

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -8,8 +8,8 @@ const siteConfig: SiteConfig = {
         src: avatar,
         alt: 'Ethan Donovan'
     },
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Felipe López',
+    subtitle: 'Here it is my personal blog',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -29,8 +29,8 @@ const siteConfig: SiteConfig = {
             href: '/blog'
         },
         {
-            text: 'Tags',
-            href: '/tags'
+            text: 'Github',
+            href: 'https://github.com/Philyppe'
         }
     ],
     footerNavLinks: [
@@ -45,29 +45,17 @@ const siteConfig: SiteConfig = {
         {
             text: 'Terms',
             href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
         }
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
             text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            href: 'https://www.instagram.com/pipe__.__/'
         }
     ],
     hero: {
         title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence.\nMy approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products.\nI have a profound appreciation for top-notch software, visual design, and the principles of product-led growth.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/JustGoodUI/dante-astro-theme) or follow me on [Twitter/X](https://twitter.com/justgoodui).",
+        text: "I'm **Felipe López **, a System engeneer student, dedicated to developing in java.\nMy approach involves embracing the MBPSC to make any System or implementsation of them.\nI have Expereince with spring boot framework, developing of front end with primefaces, and following the structure of MVC.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/Philyppe),
         image: {
             src: hero,
             alt: 'A person sitting at a desk in front of a computer'
