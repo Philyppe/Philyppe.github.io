4da95700a291926752bf6e186e6aa4d22c404c30
Felipe <lopezalmanzafelipe@gmail.com>
2026-02-20 14:05:51 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-import siteConfig from './src/data/site-config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://philyppe.github.io',
+  base: '/',
   output: 'static',
   integrations: [mdx(), sitemap()],
   vite: {
